65dbe6690a88c9ee45c960cedaebb48cfa69f89c
Vlad <vladandryschenko98@gmail.com>
2024-08-05 13:20:30 +0300

    Package: add package structure

--- app/src/main/java/com/asiaegypt/adventu/ui/splash/MainActivity.kt
+++ app/src/main/java/com/asiaegypt/adventu/ui/splash/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.asiaegypt.adventu
+package com.asiaegypt.adventu.ui.splash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.asiaegypt.adventu.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
