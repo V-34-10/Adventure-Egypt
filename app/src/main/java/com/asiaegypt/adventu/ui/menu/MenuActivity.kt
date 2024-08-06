2ce579433ef827d23d80512e148b85e5e219cd7b
Vlad <vladandryschenko98@gmail.com>
2024-08-06 11:43:17 +0300

    MenuActivity: add musicStartMode

--- app/src/main/java/com/asiaegypt/adventu/ui/menu/MenuActivity.kt
+++ app/src/main/java/com/asiaegypt/adventu/ui/menu/MenuActivity.kt
@@ -1,6 +1,7 @@
 package com.asiaegypt.adventu.ui.menu
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import android.view.animation.AnimationUtils
@@ -8,15 +9,22 @@ import androidx.appcompat.app.AppCompatActivity
 import com.asiaegypt.adventu.R
 import com.asiaegypt.adventu.databinding.ActivityMenuBinding
 import com.asiaegypt.adventu.ui.rules.RulesActivity
+import com.asiaegypt.adventu.ui.settings.MusicManager
+import com.asiaegypt.adventu.ui.settings.MusicStart
 import com.asiaegypt.adventu.ui.settings.SettingsActivity
 import com.asiaegypt.adventu.ui.themes.ThemeActivity
 
 class MenuActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMenuBinding.inflate(layoutInflater) }
+    private lateinit var preferences: SharedPreferences
+    private lateinit var managerMusic: MusicManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        managerMusic = MusicManager(this)
+        preferences = getSharedPreferences("AsianEgyptAdventurePref", MODE_PRIVATE)
+        MusicStart.musicStartMode(R.raw.music_menu, managerMusic, preferences)
         choiceMenuAppButton()
     }
 
