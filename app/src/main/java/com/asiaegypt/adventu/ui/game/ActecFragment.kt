2767e5f1ff05f780cf5409cc164655aada87ddfb
Vlad <vladandryschenko98@gmail.com>
2024-08-06 16:45:52 +0300

    Actec/Egypt/AsianFragment: add initControlBarGame: btnPlay, btnHighScore

---
+++ app/src/main/java/com/asiaegypt/adventu/ui/game/ActecFragment.kt
@@ -0,0 +1,82 @@
+package com.asiaegypt.adventu.ui.game
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.asiaegypt.adventu.R
+import com.asiaegypt.adventu.databinding.FragmentActecBinding
+import com.asiaegypt.adventu.ui.score.HighScoreActivity
+import com.asiaegypt.adventu.ui.settings.MusicManager
+import com.asiaegypt.adventu.ui.settings.MusicStart
+
+class ActecFragment : Fragment() {
+    private lateinit var binding: FragmentActecBinding
+    private lateinit var preferences: SharedPreferences
+    private lateinit var managerMusic: MusicManager
+    private var choiceLevelPairGame: String = ""
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentActecBinding.inflate(layoutInflater, container, false)
+
+        managerMusic = context?.let { MusicManager(it) }!!
+        preferences =
+            requireActivity().getSharedPreferences(
+                "AsianEgyptAdventurePref",
+                AppCompatActivity.MODE_PRIVATE
+            )
+
+        return binding.root
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        MusicStart.musicStartMode(R.raw.music_actec, managerMusic, preferences)
+
+        choiceLevelPairGame = preferences.getString("levelFindPair", "").toString()
+        binding.textLevel.text = "Level\\n$choiceLevelPairGame"
+
+        context?.let { ManagerFindPair.initFindPairGame(binding, it) }
+        initControlBarGame()
+    }
+
+    private fun initControlBarGame() {
+        var animationButton = AnimationUtils.loadAnimation(context, R.anim.scale_animation)
+        binding.btnPlay.setOnClickListener {
+            it.startAnimation(animationButton)
+            ManagerFindPair.resetFindPairGame()
+        }
+        binding.btnHighScore.setOnClickListener {
+            animationButton = AnimationUtils.loadAnimation(context, R.anim.scale_animation)
+            it.startAnimation(animationButton)
+            ManagerFindPair.resetFindPairGame()
+            startActivity(Intent(context, HighScoreActivity::class.java))
+            activity?.finish()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        managerMusic.resume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        managerMusic.pause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        managerMusic.release()
+    }
+}
