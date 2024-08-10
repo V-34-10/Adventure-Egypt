63b1ecd92c98aa7b8cccab30642fe3416bc1b483
Vlad <vladandryschenko98@gmail.com>
2024-08-10 21:21:27 +0300

    NavigationManager: add checkVersionAPA shouldHideNavigationBar, handleNavigationBarVisibility

---
+++ app/src/main/java/com/asiaegypt/adventu/NavigationManager.kt
@@ -0,0 +1,26 @@
+package com.asiaegypt.adventu
+
+import android.app.Activity
+import android.content.Context
+import android.content.res.Configuration
+import android.os.Build
+import android.view.View
+
+object NavigationManager {
+
+    private fun shouldHideNavigationBar(context: Context): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val config = context.resources.configuration
+            config.navigation == Configuration.NAVIGATION_NONAV
+        } else {
+            false
+        }
+    }
+
+    fun handleNavigationBarVisibility(context: Context) {
+        if (shouldHideNavigationBar(context)) {
+            (context as Activity).window.decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        }
+    }
+}
