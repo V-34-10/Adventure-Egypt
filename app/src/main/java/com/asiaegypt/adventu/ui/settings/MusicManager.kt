d114eb7dec3afffc8b1e42ab8c8336240afdfebd
Vlad <vladandryschenko98@gmail.com>
2024-08-16 20:56:58 +0300

    MusicManager: optimization method play: add createMediaPlayer, releaseMedia, optimization methods pause, resume, checkPlaying

--- app/src/main/java/com/asiaegypt/adventu/ui/settings/MusicManager.kt
+++ app/src/main/java/com/asiaegypt/adventu/ui/settings/MusicManager.kt
@@ -15,22 +15,13 @@ class MusicManager(private val context: Context) {
     private val scope = CoroutineScope(Job() + Dispatchers.Main)
 
     fun play(@RawRes soundResId: Int, loop: Boolean = false) {
-        if (media.containsKey(soundResId) && media[soundResId]?.isPlaying == true) {
-            return
-        }
-
-        val mediaPlayer = MediaPlayer.create(context, soundResId).apply {
-            isLooping = loop
-            setOnCompletionListener { if (!loop) scope.launch { releaseMedia(soundResId) } }
-            start()
-        }
-
-        media[soundResId] = mediaPlayer
+        val mediaPlayer = media[soundResId] ?: createMediaPlayer(soundResId, loop)
+        if (!mediaPlayer.isPlaying) mediaPlayer.start()
     }
 
-    fun pause() = media.values.forEach { if (it.isPlaying) it.pause() }
+    fun pause() = media.values.forEach(MediaPlayer::pauseIfPlaying)
 
-    fun resume() = media.values.forEach { if (!it.isPlaying) it.start() }
+    fun resume() = media.values.forEach(MediaPlayer::startIfNotPlaying)
 
     fun checkPlaying(): Boolean = media.values.any { it.isPlaying }
 
@@ -40,13 +31,30 @@ class MusicManager(private val context: Context) {
         media.clear()
     }
 
-    private fun releaseMedia(soundResId: Int) {
-        media[soundResId]?.run {
-            if (isPlaying) stop()
-            release()
-            media.remove(soundResId)
+    private fun createMediaPlayer(@RawRes soundResId: Int, loop: Boolean): MediaPlayer {
+        return MediaPlayer.create(context, soundResId).apply {
+            isLooping = loop
+            setOnCompletionListener { if (!loop) scope.launch { releaseMedia(soundResId) } }
+            media[soundResId] = this
         }
     }
+
+    private fun releaseMedia(soundResId: Int) {
+        media.remove(soundResId)?.releaseIfPlaying()
+    }
+}
+
+private fun MediaPlayer.pauseIfPlaying() {
+    if (isPlaying) pause()
+}
+
+private fun MediaPlayer.startIfNotPlaying() {
+    if (!isPlaying) start()
+}
+
+private fun MediaPlayer.releaseIfPlaying() {
+    if (isPlaying) stop()
+    release()
 }
 
 object MusicStart {
