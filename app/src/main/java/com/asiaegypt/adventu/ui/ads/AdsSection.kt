dc0efe23960397c3932d7668c7ced73a4871565a
Vlad <vladandryschenko98@gmail.com>
2024-09-11 23:27:29 +0300

    Ads: remove last version banner, add new appodeal: AdsSection, use ads in SlashScreen

--- app/src/main/java/com/asiaegypt/adventu/ui/ads/AdsSection.kt
+++ app/src/main/java/com/asiaegypt/adventu/ui/ads/AdsSection.kt
@@ -1,164 +1,41 @@
 package com.asiaegypt.adventu.ui.ads
 
-import android.app.Activity
-import android.content.Context
-import android.content.Intent
-import android.content.SharedPreferences
-import android.graphics.BitmapFactory
-import android.net.Uri
-import android.util.Log
-import android.view.View
-import com.asiaegypt.adventu.databinding.ActivityMainBinding
-import com.asiaegypt.adventu.ui.menu.MenuActivity
-import okhttp3.Call
-import okhttp3.Callback
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.Response
-import org.json.JSONException
-import org.json.JSONObject
-import java.io.IOException
-import java.util.concurrent.CountDownLatch
-import java.util.concurrent.TimeUnit
+import androidx.appcompat.app.AppCompatActivity
+import com.appodeal.ads.Appodeal
+import com.appodeal.ads.InterstitialCallbacks
+import com.asiaegypt.adventu.ui.splash.MainActivity
 
-class AdsSection(
-    private val activity: Activity,
-    private val binding: ActivityMainBinding
-) : AdsService {
-
-    private var linkAds: String = "https://on.kabushinoko.com/interstitial"
-    private var preferences: SharedPreferences =
-        activity.getSharedPreferences("AsianEgyptAdventurePref", Context.MODE_PRIVATE)
-
-    private lateinit var cookiesAds: String
-    private lateinit var userAgentAds: String
-
-    private var callAds: Call? = null
-    private var isTimeOverResponse = false
-    private var latchTime = CountDownLatch(1)
-    override fun fetchAndLoadAds() {
-        val client = createClient()
-        val request = createRequest(linkAds)
-        callAds = client.newCall(request)
-        callAds?.enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) = handleFailure(call, e)
-            override fun onResponse(call: Call, response: Response) = handleResponse(call, response)
-        })
-        checkTimeOut()
-    }
-
-    private fun createClient(): OkHttpClient =
-        OkHttpClient.Builder().callTimeout(10, TimeUnit.SECONDS).build()
-
-    private fun createRequest(url: String): Request = Request.Builder().url(url).build()
-
-    private fun handleFailure(call: Call, e: IOException) {
-        if (call.isCanceled()) {
-            Log.i("AdsSection", "Call was cancelled")
-        } else {
-            e.printStackTrace()
-        }
-        latchTime.countDown()
-    }
-
-    private fun handleResponse(call: Call, response: Response) {
-        if (call.isCanceled()) {
-            Log.i("AdsSection", "Call was cancelled")
-        } else {
-            response.use {
-                if (!response.isSuccessful) throw IOException("Unexpected code $response")
-                cookiesAds = response.header("Set-Cookie") ?: ""
-                userAgentAds = response.header("User-Agent") ?: ""
-                response.body?.string()?.let { json ->
-                    parseJsonAndLoadImage(json, cookiesAds, userAgentAds)
+object AdsSection {
+    private var interstitialShowed = false
+    fun loadAds(activity: AppCompatActivity) {
+        Appodeal.setInterstitialCallbacks(object : InterstitialCallbacks {
+            override fun onInterstitialLoaded(isPrecache: Boolean) {
+                if (!interstitialShowed) {
+                    Appodeal.show(activity, Appodeal.INTERSTITIAL)
                 }
             }
-        }
-        latchTime.countDown()
-    }
 
-    private fun checkTimeOut() {
-        if (!latchTime.await(10, TimeUnit.SECONDS)) {
-            isTimeOverResponse = true
-            callAds?.cancel()
-            loadingMenuApp()
-        }
-    }
-
-    override fun loadingMenuApp() {
-        activity.runOnUiThread {
-            activity.startActivity(Intent(activity, MenuActivity::class.java))
-        }
-        activity.finish()
-    }
-
-    override fun cancel() {
-        if (callAds?.isCanceled() == false) callAds?.cancel()
-    }
-
-    private fun parseJsonAndLoadImage(json: String, cookies: String?, userAgent: String?) {
-        try {
-            val jsonObject = JSONObject(json)
-            val actionUrl = jsonObject.getString("action")
-            val sourceUrl = jsonObject.getString("source")
-
-            saveAdsData(actionUrl, sourceUrl, cookies, userAgent)
-            loadImageAds(sourceUrl, actionUrl, cookies, userAgent)
-        } catch (e: JSONException) {
-            e.printStackTrace()
-        }
-    }
-
-    private fun saveAdsData(
-        actionUrl: String,
-        sourceUrl: String,
-        cookies: String?,
-        userAgent: String?
-    ) {
-        preferences.edit().apply {
-            putString("actionUrl", actionUrl)
-            putString("sourceUrl", sourceUrl)
-            putString("cookies", cookies)
-            putString("userAgent", userAgent)
-            apply()
-        }
-    }
-
-    override fun loadImageAds(
-        imageUrl: String,
-        actionUrl: String,
-        cookies: String?,
-        userAgent: String?
-    ) {
-        val client = createClient()
-        val request = createRequest(imageUrl)
-            .newBuilder()
-            .header("Cookie", cookies.toString())
-            .header("User-Agent", userAgent.toString())
-            .build()
-        callAds = client.newCall(request)
-        callAds?.enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) = handleFailure(call, e)
-            override fun onResponse(call: Call, response: Response) =
-                handleImageResponse(response, actionUrl)
-        })
-        checkTimeOut()
-    }
-
-    private fun handleImageResponse(response: Response, actionUrl: String) {
-        response.use {
-            if (!response.isSuccessful) throw IOException("Unexpected code $response")
-            val bitmap = response.body?.byteStream()?.let { BitmapFactory.decodeStream(it) }
-            activity.runOnUiThread {
-                if (!isTimeOverResponse && bitmap != null) {
-                    binding.resBanner.visibility = View.VISIBLE
-                    binding.resBanner.setImageBitmap(bitmap)
-                    binding.resBanner.setOnClickListener {
-                        activity.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(actionUrl)))
-                    }
-                }
+            override fun onInterstitialFailedToLoad() {
+                (activity as MainActivity).startToMenu()
             }
-        }
-        latchTime.countDown()
+
+            override fun onInterstitialShown() {
+                interstitialShowed = true
+            }
+
+            override fun onInterstitialShowFailed() {
+                (activity as MainActivity).startToMenu()
+            }
+
+            override fun onInterstitialClosed() {
+                (activity as MainActivity).startToMenu()
+            }
+
+            override fun onInterstitialClicked() {}
+
+            override fun onInterstitialExpired() {
+                Appodeal.cache(activity, Appodeal.INTERSTITIAL)
+            }
+        })
     }
 }
--- app/src/main/java/com/asiaegypt/adventu/ui/splash/MainActivity.kt
+++ app/src/main/java/com/asiaegypt/adventu/ui/splash/MainActivity.kt
@@ -3,47 +3,41 @@ package com.asiaegypt.adventu.ui.splash
 import android.animation.ValueAnimator
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
 import com.asiaegypt.adventu.NavigationManager
-import com.asiaegypt.adventu.NetworkManager
 import com.asiaegypt.adventu.databinding.ActivityMainBinding
-import com.asiaegypt.adventu.ui.ads.AdsSection
-import com.asiaegypt.adventu.ui.ads.AdsService
+import com.asiaegypt.adventu.ui.ads.AdsSection.loadAds
 import com.asiaegypt.adventu.ui.menu.MenuActivity
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-    private lateinit var adsService: AdsService
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         NavigationManager.handleNavigationBarVisibility(this)
-        adsService = AdsSection(this, binding)
-        loadingApp()
+
+        CoroutineScope(Dispatchers.Main).launch {
+            loadingApp()
+        }
     }
 
-    private fun loadingApp() {
-        val animationDuration = 3000L
-        animationLoading(animationDuration)
-        val isInternetAvailable = NetworkManager.checkInternet(this)
-        lifecycleScope.launch {
-            delay(animationDuration)
-            if (isInternetAvailable) {
-                loadAds()
-            } else {
-                startActivity(Intent(this@MainActivity, MenuActivity::class.java))
-                finish()
+    private suspend fun loadingApp() {
+        withContext(Dispatchers.IO) {
+            try {
+                loadAds(this@MainActivity)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                startToMenu()
             }
         }
     }
 
     private fun animationLoading(duration: Long) {
         val displayMetrics = resources.displayMetrics
-        val screenWidth = displayMetrics.widthPixels
         val progressBarWidth = (480 * displayMetrics.density).toInt()
         val maxAnimationWidth = progressBarWidth - 10
 
@@ -60,28 +54,15 @@ class MainActivity : AppCompatActivity() {
         animation.start()
     }
 
-    private fun loadAds() {
-        val preferences = getSharedPreferences("AsianEgyptAdventurePref", MODE_PRIVATE)
-        if (preferences.contains("cookies") && preferences.contains("userAgent")) {
-            val action = preferences.getString("actionUrl", "")
-            val source = preferences.getString("sourceUrl", "")
-            val cookies = preferences.getString("cookies", "")
-            val userAgent = preferences.getString("userAgent", "")
-            adsService.loadImageAds(source.toString(), action.toString(), cookies, userAgent)
-
-        } else {
-            adsService.fetchAndLoadAds()
-        }
+    fun startToMenu() {
+        animationLoading(3000L)
+        startActivity(Intent(this@MainActivity, MenuActivity::class.java))
+        finish()
     }
 
     @Deprecated("Deprecated in Java")
     override fun onBackPressed() {
         super.onBackPressed()
         finish()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        adsService.cancel()
     }
 }
